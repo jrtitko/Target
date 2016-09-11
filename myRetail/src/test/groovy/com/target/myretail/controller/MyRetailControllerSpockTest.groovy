653f89fb61cfb810864f298bfe7dd98d620e9db6
J.R. Titko <j.r.titko.professional@gmail.com>
2016-09-11 12:11:26 -0500

    added logic for accessing the web application context

--- myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
+++ myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
@@ -3,13 +3,11 @@ package com.target.myretail.controller
 import com.target.myretail.MyRetailApplication
 import com.target.myretail.model.Price
 import com.target.myretail.repositories.PriceRepository
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
@@ -34,28 +32,69 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 
 @SpringApplicationConfiguration(classes = MyRetailApplication.class)
+@WebAppConfiguration
 class MyRetailControllerSpockTest extends Specification {
 
+    MockMvc mockMvc
+
+    @SuppressWarnings("rawtypes")
+    HttpMessageConverter mappingJackson2HttpMessageConverter;
+
     @Autowired
-    PriceRepository priceRepository;
+    WebApplicationContext webApplicationContext;
+
+    MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Autowired
+    private PriceRepository priceRepository;
+
+    @Autowired
+    MyRetailController myRetailController;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+//        mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+//                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        mappingJackson2HttpMessageConverter = converters.find {
+            it.class.isAssignableFrom(MappingJackson2HttpMessageConverter)
+        }
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
 
     Price origPrice;
 
     def setup() {
+        mockMvc = webAppContextSetup(webApplicationContext).build()
+
         origPrice = priceRepository.findOne(13860428);
         priceRepository.save(new Price(13860428,  13.49, 'USD'));
     }
 
-//    def cleanup() {
-//        if (origPrice != null) {
-//            priceRepository.save(origPrice);
-//        } else {
-//            priceRepository.delete(13860428);
-//        }
-//    }
+    def cleanup() {
+        if (origPrice != null) {
+            priceRepository.save(origPrice);
+        } else {
+            priceRepository.delete(13860428);
+        }
+    }
 
     def "get product"() {
 
+//        mockMvc.perform(get('/myRetail/products/13860428'))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(contentType))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(jsonPath('$.id', is(13860428)))
+//                .andExpect(jsonPath('$.name', is('The Big Lebowski (Blu-ray)')))
+//                .andExpect(jsonPath('$.current_price', notNullValue()))
+//                .andExpect(jsonPath('$.current_price.value', is(13.49)))
+//                .andExpect(jsonPath('$.current_price.currency_code', is('USD')))
     }
 
     @Unroll
