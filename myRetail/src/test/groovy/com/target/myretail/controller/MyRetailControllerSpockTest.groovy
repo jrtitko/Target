2a4696086d0db73b7faf8d4382346378a9e562a0
J.R. Titko <j.r.titko.professional@gmail.com>
2016-09-11 12:08:24 -0500

    added spock-spring to the build.gradle

--- myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
+++ myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
@@ -3,11 +3,13 @@ package com.target.myretail.controller
 import com.target.myretail.MyRetailApplication
 import com.target.myretail.model.Price
 import com.target.myretail.repositories.PriceRepository
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
