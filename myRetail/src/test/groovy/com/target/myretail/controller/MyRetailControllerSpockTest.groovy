09fbcd3ce92074c9af620069ffca967ac5afc23a
J.R. Titko <j.r.titko.professional@gmail.com>
2016-09-12 06:44:57 -0500

    Added the remaining tests for the controller

--- myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
+++ myRetail/src/test/groovy/com/target/myretail/controller/MyRetailControllerSpockTest.groovy
@@ -7,13 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.boot.test.WebIntegrationTest
 import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.mock.http.MockHttpOutputMessage
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
-import static org.hamcrest.Matchers.is
-import static org.hamcrest.Matchers.notNullValue
+import static org.hamcrest.Matchers.*
+import static org.junit.Assert.assertNotNull
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 /**
@@ -30,9 +34,27 @@ class MyRetailControllerSpockTest extends Specification {
     @Autowired
     MyRetailController myRetailController;
 
+    def mockMvc
+
     Price origPrice;
 
+    @SuppressWarnings("rawtypes")
+    HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        mappingJackson2HttpMessageConverter = converters.find {
+            it instanceof MappingJackson2HttpMessageConverter
+        }
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
     def setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(myRetailController).build()
+
         origPrice = priceRepository.findOne(13860428);
         priceRepository.save(new Price(13860428,  13.49, 'USD'));
     }
@@ -45,10 +67,7 @@ class MyRetailControllerSpockTest extends Specification {
         }
     }
 
-    def "get product"() {
-        given:
-        def mockMvc = MockMvcBuilders.standaloneSetup(myRetailController).build()
-
+    def "GET product"() {
         when:
         def response = mockMvc.perform(get("/myRetail/products/13860428"))
 
@@ -63,4 +82,82 @@ class MyRetailControllerSpockTest extends Specification {
             .andExpect(jsonPath('$.current_price.value', is(13.49d)))
             .andExpect(jsonPath('$.current_price.currency_code', is('USD')))
     }
+
+    def "GET product where the product does not exist at the remote site"() {
+        when:
+        def response = mockMvc.perform(get("/myRetail/products/99999999"))
+
+        then:
+        response
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath('$.id', is(99999999)))
+            .andExpect(jsonPath('$').value(not(hasKey('name'))))
+            .andExpect(jsonPath('$').value(not(hasKey('current_price'))))
+    }
+
+    def "GET product where the price does not exist in our databaes"() {
+        expect:
+        mockMvc.perform(get("/myRetail/products/16752456"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath('$.id', is(16752456)))
+                .andExpect(jsonPath('$.name', is('Lego&reg; Super Heroes The Tumbler 76023')))
+                .andExpect(jsonPath('$').value(not(hasKey('current_price'))))
+    }
+
+    def "POST product"() {
+        given:
+        def productId = 13860428
+        Price newPrice = new Price(13860428, 99.99, "USD")
+
+        when:
+        def response = mockMvc.perform(post("/myRetail/products/" + productId)
+                .content(json(newPrice))
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+
+        then:
+        response.andExpect(status().isOk());
+
+        when:
+        Price afterPrice = priceRepository.findOne(productId);
+
+        then:
+        afterPrice != null
+        afterPrice.getValue() == 99.99
+    }
+
+    def "POST product with mismatched price id"() {
+        given:
+        def productId = 13860428
+        Price beforePrice = priceRepository.findOne(productId)
+        Price newPrice = new Price(99999999, 99.99, "USD");
+
+        when:
+        def response = mockMvc.perform(post("/myRetail/products/" + productId)
+                .content(json(newPrice))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError())
+        then:
+        def e = thrown(Exception)
+        e.getCause().getMessage().contains("A price update")
+
+        when:
+        Price afterPrice = priceRepository.findOne(productId)
+
+        then:
+        afterPrice != null
+        beforePrice.getValue() == afterPrice.getValue()
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+
 }
